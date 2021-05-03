a0560df3c96ec09b5a90e572aae54b3da7f12db2
AarushJain <70953666+AarushJain@users.noreply.github.com>
2021-05-03 19:00:40 +0530

    Add files via upload

---
+++ project 24r/Dustbin.js
@@ -0,0 +1,23 @@
+class Dustbin{
+    constructor(x, y, width, height){
+
+    var options={
+        isStatic:true,
+    }
+    this.x=x;
+    this.y=y;
+    this.width=width;
+    this.height=height;
+    this.body=Bodies.rectangle(this.x,this.y,this.width,this.height,options);
+    World.add(world,this.body);
+    }
+
+    display(){
+        var binPos=this.body.position; 
+        push();
+        translate(binPos.x,binPos.y);
+        rect(0,0,this.width,this.height);
+        pop();
+        }
+
+    }
---
+++ project 24r/sketch.js
@@ -0,0 +1,64 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var paperBall;
+var box1,box2,box3;
+var ground;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	paperBall= new Paper(50,350,70);
+
+	box1= new Dustbin(430,600,150,20);
+	box2= new Dustbin(350,height-140,20,100);
+	box3= new Dustbin(500,height-140,20,100);
+
+	ground= new Ground(width/2, 600, width, 20);
+
+	//Create the Bodies Here.
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background('pink');
+
+  
+  	paperBall.display();
+	
+
+	ground.display();
+	
+	box1.display();
+	box2.display();
+	box3.display();
+
+  drawSprites();
+ 
+}
+
+function keyPressed(){
+	if(keyCode===UP_ARROW){	
+	Matter.Body.applyForce(paperBall.body, paperBall.body.position,{x:130, y:-200});	
+	}	
+	
+	}
+
